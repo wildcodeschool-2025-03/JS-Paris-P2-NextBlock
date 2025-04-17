f416368c1bdcc508cb127f60ac239e756e1a5069
Brian Boudrioux <brian.boudrioux@wildcodeschool.com>
2025-04-17 15:16:19 +0200

    setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import { Outlet } from "react-router";
 
 function App() {
 	const name = "toto";
 	const text = "Welcome";
 	return (
 		<>
-			{text} {name}
+			<Outlet />
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root') || document.body)
-.render(<App />);
+const router = createBrowserRouter([
+	{
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+		],
+	},
+]);
+
+createRoot(document.getElementById("root") || document.body).render(
+	<RouterProvider router={router} />,
+);
